eba06fe620aa2bd5371c467b30f6297b3600b979
rahulrk1991 <rahulrk1991@gmail.com>
2018-07-11 14:04:40 +0530

    Learnt Templates, url namespacing and get_object_or_404 shortcut

--- music/views.py
+++ music/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album
 
 
 def index(request):
-    return HttpResponse("<h1> This is the music index page</h1>")
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
 
 
