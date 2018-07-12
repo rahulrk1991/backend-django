27f1b3e67f151ab45f400294aa4c6cd959c12fb4
rahulrk1991 <rahulrk1991@gmail.com>
2018-07-12 14:38:46 +0530

    Generic views, navigation menu

--- music/views.py
+++ music/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album
+from django.views import generic
+from models import Album
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albums': all_albums})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
